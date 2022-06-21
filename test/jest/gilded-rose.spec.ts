126e9d5e71fd8df02d76cbce7df72bd02668fb8d
codefingas <udohezekiel34@yahoo.com>
2022-06-21 04:27:42 +0100

    test: can add item to glided rose

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,11 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
+  it('should add new item', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+    const item = gildedRose.items[0];
+    expect(item.name).toBe('foo');
+    expect(item.sellIn).toBe(0);
+    expect(item.quality).toBe(0);
   });
 });
